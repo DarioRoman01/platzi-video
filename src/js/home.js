239a1107a239ca3b29e1348c341a425c0f6d2aa1
DarioRoman <darioroman2001@gmail.com>
2021-02-22 16:57:02 -0300

    create methods to generate templates and render them.

--- src/js/home.js
+++ src/js/home.js
@@ -1,5 +1,22 @@
 
 (async function load() {
+
+  // document elements
+  const $actionContainer = document.querySelector('#action');
+  const $dramaContainer = document.getElementById('drama');
+  const $animationContainer = document.getElementById('animation');
+
+  const $featuringContainer = document.getElementById('#featuring');
+  const $form = document.getElementById('#form');
+  const $home = document.getElementById('#home');
+
+  const $modal = document.getElementById('modal');
+  const $overlay = document.getElementById('overlay');
+  const $hideModal = document.getElementById('hide-modal');
+
+  const $modalTitle =  $modal.querySelector('h1');
+  const $modalImage =  $modal.querySelector('img');
+  const $modalDescription = $modal.querySelector('p');
   
   // get the data of the api
   async function getData(url) {
@@ -22,35 +39,31 @@
     )
   }
 
+  // Generate the template with the data
+  function createTemplate(HTMLString) {
+    const html = document.implementation.createHTMLDocument();
+    html.body.innerHTML = HTMLString;
+    return html.body.children[0];
+  }
+
+  // Insert the templates in the DOM
+  function renderMovieList(list, $container) {
+    $container.children[0].remove();
+    list.forEach((movie) => {
+      const HTMLString = videoItemTemplate(movie);
+      const movieElement = createTemplate(HTMLString);
+      $container.append(movieElement);
+    });
+  }
+
   // create variables of list of movies based on genres
   const actionList =  await getData('https://yts.mx/api/v2/list_movies.json?genre=action');
-  const drameList = await getData('https://yts.mx/api/v2/list_movies.json?genre=drama');
+  const dramaList = await getData('https://yts.mx/api/v2/list_movies.json?genre=drama');
   const animationList = await getData('https://yts.mx/api/v2/list_movies.json?genre=animation');
 
-  debugger
-
-  actionList.data.movies.forEach((movie) => {
-    const HTMLString = videoItemTemplate(movie);
-    console.log(HTMLString)
-  });
-
-  const $actionContainer = document.querySelector('#action');
-  const $dramaContainer = document.getElementById('#drama');
-  const $animationContainer = document.getElementById('#animation');
-
-  const $featuringContainer = document.getElementById('#featuring');
-  const $form = document.getElementById('#form');
-  const $home = document.getElementById('#home');
-
-  const $modal = document.getElementById('modal');
-  const $overlay = document.getElementById('overlay');
-  const $hideModal = document.getElementById('hide-modal');
-
-  const $modalTitle =  $modal.querySelector('h1');
-  const $modalImage =  $modal.querySelector('img');
-  const $modalDescription = $modal.querySelector('p');
-
-
+  renderMovieList(actionList.data.movies, $actionContainer);
+  renderMovieList(dramaList.data.movies, $dramaContainer);
+  renderMovieList(animationList.data.movies, $animationContainer);
 
 })()
 
