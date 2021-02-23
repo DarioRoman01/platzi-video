2bd93590cde611ce4a2efbb1117f943b1cfa7da8
DarioRoman <darioroman2001@gmail.com>
2021-02-23 10:11:36 -0300

    add more events listener to the page.

--- src/js/home.js
+++ src/js/home.js
@@ -6,9 +6,9 @@
   const $dramaContainer = document.getElementById('drama');
   const $animationContainer = document.getElementById('animation');
 
-  const $featuringContainer = document.getElementById('#featuring');
-  const $form = document.getElementById('#form');
-  const $home = document.getElementById('#home');
+  const $featuringContainer = document.getElementById('featuring');
+  const $form = document.getElementById('form');
+  const $home = document.getElementById('home');
 
   const $modal = document.getElementById('modal');
   const $overlay = document.getElementById('overlay');
@@ -18,6 +18,49 @@
   const $modalImage =  $modal.querySelector('img');
   const $modalDescription = $modal.querySelector('p');
   
+  // add attributes to a element
+  function setAttributes($element, attributes) {
+    for (const attribute in attributes) {
+      $element.setAttribute(attribute, attributes[attribute]);
+    }
+  }
+
+  // event to be executed when the user search for a movie
+  $form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    $home.classList.add('search-active');
+    const $loader = document.createElement('img');
+
+    setAttributes($loader, {
+      src: 'src/images/loader.gif',
+      height: 50,
+      width: 50,
+    })
+
+    $featuringContainer.append($loader);
+  })
+
+  // event to be executed when the user click on a movie
+  function showModal() {
+    $overlay.classList.add('active');
+    $modal.style.animation = 'modalIn .8s forwards';
+  }
+
+  function hideModal() {
+    $overlay.classList.remove('active');
+    $modal.style.animation = 'modalOut .8s forwards'
+  }
+
+  $hideModal.addEventListener('click', () => {
+    hideModal();
+  });
+
+  function addEventClick($element) {
+    $element.addEventListener('click', () => {
+      showModal();
+    })
+  }
+
   // get the data of the api
   async function getData(url) {
     const response = await fetch(url)
@@ -53,6 +96,7 @@
       const HTMLString = videoItemTemplate(movie);
       const movieElement = createTemplate(HTMLString);
       $container.append(movieElement);
+      addEventClick(movieElement)
     });
   }
 
